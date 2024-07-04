3b50caf73c82ed8225884c3a289471df84ddebf1
potae4nn <potae4nn@gmail.com>
2024-07-04 16:14:44 +0700

    add mdx blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withMDX from '@next/mdx';
 
-export default nextConfig;
+const withMDXConfig = withMDX({
+  extension: /\.mdx?$/,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+};
+
+export default withMDXConfig(nextConfig);
